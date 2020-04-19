832a77f77cdf21f87b7100b3a5ea943e8ccf58d9
oddlyspaced <oddlyspaced1@gmail.com>
2020-04-19 18:22:13 +0530

    Create dashboard activity

---
+++ app/src/main/java/com/oddlyspaced/covid19india/activity/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.oddlyspaced.covid19india.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.covid19india.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}

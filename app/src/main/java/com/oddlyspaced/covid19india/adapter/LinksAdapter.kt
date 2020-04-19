7f8d657d46a13d9b44010ac28e2b6d70c8671494
oddlyspaced <oddlyspaced1@gmail.com>
2020-04-19 19:53:15 +0530

    Highlight links specifically

--- app/src/main/java/com/oddlyspaced/covid19india/adapter/LinksAdapter.kt
+++ app/src/main/java/com/oddlyspaced/covid19india/adapter/LinksAdapter.kt
@@ -1,19 +1,20 @@
 package com.oddlyspaced.covid19india.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.text.Spannable
+import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.oddlyspaced.covid19india.R
-import com.oddlyspaced.covid19india.modal.FaqItem
 import com.oddlyspaced.covid19india.modal.LinksItem
-import com.oddlyspaced.covid19india.modal.StatContainerItem
-import java.text.NumberFormat
-import java.util.*
-import kotlin.collections.ArrayList
+
 
 class LinksAdapter(private val list: ArrayList<LinksItem>, private val context: Context): RecyclerView.Adapter<LinksAdapter.ViewHolder>() {
 
@@ -32,7 +33,16 @@ class LinksAdapter(private val list: ArrayList<LinksItem>, private val context: 
         val item = list[position]
 
         holder.title.text = item.title
-        holder.touch.text = item.link
+
+        val spanText: Spannable = Spannable.Factory.getInstance().newSpannable(item.link.toLowerCase())
+        spanText.setSpan(BackgroundColorSpan(context.getColor(R.color.colorBlueLight)), 0, item.link.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        holder.touch.text = spanText
+
+        holder.touch.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(item.link.toLowerCase())
+            context.startActivity(intent)
+        }
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {

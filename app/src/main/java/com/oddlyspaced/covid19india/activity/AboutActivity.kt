e481997d7094667d8e6f775eb097e714d5247718
oddlyspaced <oddlyspaced1@gmail.com>
2020-04-23 13:43:37 +0530

    Implement simplified about activity

--- app/src/main/java/com/oddlyspaced/covid19india/activity/AboutActivity.kt
+++ app/src/main/java/com/oddlyspaced/covid19india/activity/AboutActivity.kt
@@ -1,6 +1,7 @@
 package com.oddlyspaced.covid19india.activity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,7 +16,18 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         setupBottomNavigation()
-        loadFaq()
+
+        viewTouchGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/oddlyspaced/covid19-android")
+            startActivity(intent)
+        }
+
+        viewTouchCovid.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.covid19india.org/")
+            startActivity(intent)
+        }
     }
 
     private fun setupBottomNavigation() {
@@ -36,18 +48,4 @@ class AboutActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadFaq() {
-        val list = ArrayList<FaqItem>()
-        list.add(FaqItem("Are you official?","No"))
-        list.add(FaqItem("What are your sources? How is the data gathered for this project?","We are using state bulletins and official handles to update our numbers. The data is validated by a group of volunteers and published into a Google sheet and an API. API is available for all at api.covid19india.org. We would love it if you can use this data in the fight against this virus. The source list is here."))
-        list.add(FaqItem("Why does covid19india.org have more positive count than MoH?","MoHFW updates the data at a scheduled time. However, we update them based on state press bulletins, official (CM, Health M) handles, PBI, Press Trust of India, ANI reports. These are generally more recent."))
-        list.add(FaqItem("Where can I find the data for this?","You can preview all the data collected in this page : patientdb.covid19india.org . All the data is available through an API for further analysis and development here : api.covid19india.org"))
-        list.add(FaqItem("Who are you?","We are a group of dedicated volunteers who curate and verify the data coming from several sources. We extract the details, like a patient's relationship with other patients to identify local and community transmissions, travel history and status. We never collect or expose any personally identifiable data regarding the patients."))
-        list.add(FaqItem("Why are you guys putting in time and resources to do this while not gaining a single penny from it?","Because it affects all of us. Today it's someone else who is getting infected; tomorrow it could be us. We need to prevent the spread of this virus. We need to document the data so that people with knowledge can use this data to make informed decisions."))
-        val layoutManager = LinearLayoutManager(this)
-        recyclerFaq.setHasFixedSize(true)
-        recyclerFaq.layoutManager = layoutManager
-        val adapter = FaqAdapter(list, this)
-        recyclerFaq.adapter = adapter
-    }
 }
